0d9f4eaf57c781419ae79ca7c1fe8c1d2df57392
cpiaulet <caroline.piaulet@umontreal.ca>
2022-07-29 13:33:23 -0400

    add new S3 kwd to the example ecf files

--- src/eureka/S3_data_reduction/s3_reduce.py
+++ src/eureka/S3_data_reduction/s3_reduce.py
@@ -455,7 +455,9 @@ def reduce(eventlabel, ecf_path=None, s2_meta=None):
                 plots_s3.lc_nodriftcorr(meta, spec.wave_1d, spec.optspec,
                                         optmask=spec.optmask)
                 plots_s3.optimal_spectrum_and_std(spec, meta)
-                plots_s3.lc_SNR_inspection(spec, meta, exclude_range=meta.exclude_range, rem_lin_trend=True)
+                plots_s3.lc_SNR_inspection(spec, meta, 
+                                           exclude_range=meta.exclude_range, 
+                                           rem_lin_trend=True)
 
             # Save results
             if meta.save_output:
